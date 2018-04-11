d5d51cea0b917ebac8ab899e2d69c640452cab13
Son Roy Almerol <miyata@Sons-MacBook-Pro.local>
2018-04-11 11:39:03 +0800

    add cors

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 routes(app);
 
 var server = app.listen(3031, function () {
